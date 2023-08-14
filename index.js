504dcb83acb502a688ac63debe6b4cc005094a18
vohaiphuc <vohaiphuc@gmail.com>
2023-08-14 09:39:16 +0700

    clean code

--- index.js
+++ index.js
@@ -1,60 +1,4 @@
-// $(document).ready(function () {
-//     $('#horizontalTab').easyResponsiveTabs({
-
-//         // Types: default, vertical, accordion   
-//         type: 'default',
-
-//         //auto or any width like 600px
-//         width: 'auto',
-
-//         // 100% fit in a container
-//         fit: true,
-
-//         // Close the panels on start, 
-//         // the options 'accordion' and 'tabs' keep them closed in there respective view types
-//         closed: false,
-
-//         // The tab groups identifier
-//         tabidentify: '',
-
-//         // background color for active tabs in this group
-//         activetab_bg: 'white',
-
-//         // background color for inactive tabs in this group
-//         inactive_bg: '#F5F5F5',
-
-//         // border color for active tabs heads in this group
-//         active_border_color: '#c1c1c1',
-
-//         // border color for active tabs contect in this group 
-//         // so that it matches the tab head border
-//         active_content_border_color: '#c1c1c1',
-
-//         activate: function () { }
-
-//     });
-
-// });
-
-
-$(document).ready(function () {
-    //Horizontal Tab
-    $('#parentHorizontalTab').easyResponsiveTabs({
-        type: 'default', //Types: default, vertical, accordion
-        width: 'auto', //auto or any width like 600px
-        fit: true, // 100% fit in a container
-        tabidentify: 'hor_1', // The tab groups identifier
-        activate: function (event) { // Callback function if tab is switched
-            var $tab = $(this);
-            var $info = $('#nested-tabInfo');
-            var $name = $('span', $info);
-            $name.text($tab.text());
-            $info.show();
-        }
-    });
-});
-
-// Today res tab
+// Responsive Tab
 $('#responsiveTabsDemo').responsiveTabs({
     startCollapsed: 'accordion'
 });
@@ -76,6 +20,24 @@ $(document).ready(function () {
         }
     })
 });
+
+// Scroll to top - w3 School
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+    let mybutton = document.querySelector("#backToTop");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+// Scroll to top - END
 
 // Darkmode
 function toggleDarkmode() {
