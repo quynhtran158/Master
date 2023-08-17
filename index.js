aee30fdf862fde9f275157f3bc649d9484086ac3
quynhtran158 <quynhtran.3002@gmail.com>
2023-08-17 14:28:51 -0400

    update text-color in banner section

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 $(".counter").countUp();
+/* Darkmode */
+function toggleDarkmode() {
+  let html = document.getElementsByTagName("switchButton")[0];
+  let mode = html.getAttribute("data-bs-theme");
+  html.setAttribute("data-bs-theme", mode == "dark" ? "white" : "dark");
+}
